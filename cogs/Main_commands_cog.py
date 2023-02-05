813a9bf9e9cf70b062fd0b6f33b042af20f9abba
Yanbo Cheng <86802756+iamyanbo@users.noreply.github.com>
2023-02-05 17:42:19 -0500

    Refactor spikes code so it only has one loop get_data that calls the two seperate functions. Also added docstrings for bazaar functions

--- cogs/Main_commands_cog.py
+++ cogs/Main_commands_cog.py
@@ -19,6 +19,12 @@ class Main_commands(commands.Cog):
         self.playlist_yt = []
         self.playlist_google = []
         self.watching_list = []
+        self.channel_number = 1071237492663128124 # set this to the channel you want the bot to send the message to
+        self.spiked_items = []
+        self.spike_threshold = 2 # set this to the threshold for what multiplier you consider a spike
+        self.reasonable_buy_volume = 100 # set this to the threshold for what volume you consider a reasonable buy
+        self.reasonable_sell_volume = 100 # set this to the threshold for what volume you consider a reasonable sell
+        self.data = []
 
     @commands.command(name='rem')
     async def rem(self, ctx, *args):
@@ -37,7 +43,7 @@ class Main_commands(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Bot is online.')
-        self.spike_watcher.start()       
+        self.get_data.start()  
                 
     @commands.command(name = 'help', aliases = ['h'])
     async def help(self, ctx):
@@ -192,6 +198,9 @@ class Main_commands(commands.Cog):
             
     @commands.command()
     async def wheres(self, ctx, message):
+        """
+        Funny command to see "where" a user is.
+        """
         if message == '<@!361913675872731136>':
             user = '<@!361913675872731136>'
             await ctx.channel.send(f'{user} downstairs showering with his bri\'ish cousins playing csgo and eating and taking a LGBTQ poop')
@@ -210,6 +219,12 @@ class Main_commands(commands.Cog):
 
     @commands.command(name = 'revive')
     async def revive(self, ctx, member: discord.Member):
+        """
+        Continuously moves a member in and out of a voice channel, spamming them with sound notifications.
+        Meant to be use as a funny means to "revive" a defened or afk person.
+        
+        This command only works if the server has at least 2 voice channels.
+        """
         try:
             voice_channel = ctx.guild.voice_channels
             await member.move_to(voice_channel[1])
@@ -232,8 +247,16 @@ class Main_commands(commands.Cog):
             
     @commands.command(name = "flips")
     async def flips(self, ctx):
-        data, driver = await self.get_data()
-        reasonable_flips = await self.get_reasonable_flips(data)
+        """
+        Prints out flips that are reasonable, in the format of:
+        - Item name
+        - Buy price
+        - Buy volume
+        - Sell price
+        - Sell volume
+        - CPR(coin per hour)
+        """
+        reasonable_flips = await self.get_reasonable_flips()
         data_str = ""
         for dic in reasonable_flips:
             data_str += f"{dic.get('Item name')}\n"
@@ -242,21 +265,63 @@ class Main_commands(commands.Cog):
             data_str += f"Sell: {dic.get('Sell')}\n"
             data_str += f"Sell volume: {dic.get('1-hr instasells')}\n"
             data_str += f"CPR: {dic.get('Coins per hour')}\n\n"
-        driver.quit()
         await ctx.send(data_str)
         
-    @tasks.loop(seconds=60)
-    async def spike_watcher(self):
-        channel = await self.client.fetch_channel(1071237492663128124) # channel id goes here, this is the channel id for the test server
-        data, driver = await self.get_data()
+    async def spike_watcher(self, data):
+        """
+        Function that checks if any of the items in self.watching_list is spiking.
+        Prints out the item name and the current buy price if it is spiking, then removes it from the list.
+        """
+        channel = await self.client.fetch_channel(self.channel_number)
         # it is reasonable to assume that the first 15 items includes the spikes
         for item in self.watching_list:
             for i in range(0, 15):
+                if data[i].get("Buy") > self.spike_threshold * data[i].get("Sell"):
+                    item_name = data[i].get("Item name")
+                    if item in item_name.lower():
+                        await channel.send(f"{item_name} is spiking! Current buy price: {data[i].get('Buy')}")
+                        self.watching_list.remove(item)
+        await self.clean_spiked_items(data[:15])
+    
+    async def general_spikes(self, data):
+        """
+        Function that checks if any of the items in the top 15 are spiking.
+        Prints out the item name and the current buy price if it is spiking.
+        """
+        channel = await self.client.fetch_channel(self.channel_number)
+        for i in range(0, 15):
+            if data[i].get("Buy") > self.spike_threshold * data[i].get("Sell"):
                 item_name = data[i].get("Item name")
-                if item in item_name.lower():
-                    await channel.send(f"{item_name} is spiking!")
-        
+                if item_name not in self.spiked_items:
+                    self.spiked_items.append(item_name)
+                    await channel.send(f"{item_name} is spiking! Current buy price: {data[i].get('Buy')}")
+        await self.clean_spiked_items(data[:15])
+    
+    async def clean_spiked_items(self, top_15):
+        """
+        Function that removes items from self.spiked_items if they are no longer in the top_15, i.e. they are no longer spiking.
+        """
+        for item in self.spiked_items:
+            get_rid = True
+            for dic in top_15:
+                if item in dic.get("Item name"):
+                    get_rid = False
+            if get_rid:
+                self.spiked_items.remove(item)
+    
+    @tasks.loop(seconds=60) # change this to change how often the bot updates the data
     async def get_data(self):
+        """
+        Function that gets the data from Bazaar meta website and returns it as a list of dictionaries in the format of:
+        - Item name
+        - Buy price
+        - Buy volume
+        - Sell price
+        - Sell volume
+        - CPR(coin per hour)
+        
+        If you wish to use a different website, you can change the url and the way the data is parsed.
+        """
         url = "https://www.skyblock.bz/flips"
         print("Getting data from " + url)
         driver = webdriver.Edge()
@@ -265,10 +330,10 @@ class Main_commands(commands.Cog):
 
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
-        cards = soup.find_all("div", class_="card svelte-ln9euk")
+        cards = soup.find_all("div", class_="card svelte-ln9euk") # do not change this if you are using the same website
 
         data = []
-        for i in range(0, 35):
+        for i in range(0, 35): # change this to however many items you want to check, currently 35 as anything more makes around only 5 million coins per hour
             item_name = cards[i].find("div", class_="item-name svelte-ln9euk")
             if(item_name):
                 item_info = cards[i].find("p", class_="card_menu svelte-ln9euk")
@@ -286,12 +351,20 @@ class Main_commands(commands.Cog):
                         pass
                 data.append(temp)
                 
-        return data, driver
+        self.data = data
+        driver.quit()
+        await self.spike_watcher(data)
+        await self.general_spikes(data)
     
-    async def get_reasonable_flips(self, data):
+    async def get_reasonable_flips(self):
+        """
+        Function that returns a list of flips that are reasonable from the data.
+        Reasonable flips are flips that have a buy volume and sell volume of over self.reasonable_buy_volume and self.reasonable_sell_volume respectively. 
+        This is done so there is less competition.
+        """
         reasonable_flips = []
-        for flip in data:
-            if(flip["1-hr instabuys"] > 100 and flip["1-hr instasells"] > 100):
+        for flip in self.data:
+            if(flip["1-hr instabuys"] > self.reasonable_buy_volume and flip["1-hr instasells"] > self.reasonable_sell_volume): 
                 reasonable_flips.append(flip)
         return reasonable_flips
         
